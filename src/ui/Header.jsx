0a076401013f570b7e4bf671ef2f493021fb1d73
Albion Berisha <100076006+aberishabackend@users.noreply.github.com>
2025-01-12 11:26:50 +0100

    builded the App Layout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Food Rush Co.</Link>
+      <p>Albion</p>
+    </header>
+  );
+}
+
+export default Header;
