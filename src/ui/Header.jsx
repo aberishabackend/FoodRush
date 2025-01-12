89cf73f6f869493d7901f5a524d5bc03612228c3
Albion Berisha <100076006+aberishabackend@users.noreply.github.com>
2025-01-12 13:37:32 +0100

    fetching data for orders and error handling in CreateOrder Form

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,9 +1,12 @@
 import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
 
 function Header() {
   return (
     <header>
       <Link to="/">Food Rush Co.</Link>
+
+      <SearchOrder />
       <p>Albion</p>
     </header>
   );
