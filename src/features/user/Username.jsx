d30c0115245bb815502b7bfc9624d2d14900c29b
Albion Berisha <100076006+aberishabackend@users.noreply.github.com>
2025-01-12 17:50:54 +0100

    styling with Tailwind

---
+++ src/features/user/Username.jsx
@@ -0,0 +1,5 @@
+function Username() {
+  return <div className="hidden text-sm font-semibold md:block">Albion</div>;
+}
+
+export default Username;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,13 +1,16 @@
-import { Link } from 'react-router-dom';
-import SearchOrder from '../features/order/SearchOrder';
+import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <header>
-      <Link to="/">Food Rush Co.</Link>
+    <header className="border-stone flex items-center justify-between border-stone-200 bg-red-500 px-4 py-3 uppercase sm:px-6">
+      <Link to="/" className="tracking-widest">
+        Food Rush Co.
+      </Link>
 
       <SearchOrder />
-      <p>Albion</p>
+      <Username />
     </header>
   );
 }
